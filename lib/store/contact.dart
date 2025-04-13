1406df7c66ecd9731d1f8a9d9b8a98a0f376163c
zelvan <zelvan1701@gmail.com>
2025-04-13 22:40:10 +0700

    feat: add entrypoint for using objectbox

--- lib/store/contact.dart
+++ lib/store/contact.dart
@@ -1,1 +1,39 @@
-class 
+import 'package:database_assignment/entity/contact.dart';
+import 'package:database_assignment/entity/group.dart';
+import 'package:database_assignment/objectbox.g.dart';
+
+class ContactStore {
+  late final Store store;
+  late final Box<Contact> contactBox;
+  late final Box<Group> groupBox;
+
+  ContactStore._create(this.store) {
+    contactBox = store.box<Contact>();
+    groupBox = store.box<Group>();
+
+    if (groupBox.isEmpty()) {
+      groupBox.putMany([
+        Group(name: 'Family'),
+        Group(name: 'Friends'),
+        Group(name: 'Work'),
+      ]);
+    }
+  }
+
+  static Future<ContactStore> init(String dir) async {
+    final store = await openStore(directory: dir);
+    return ContactStore._create(store);
+  }
+
+  List<Contact> getContacts() => contactBox.getAll();
+  void addContact(Contact contact, Group group) {
+    contact.group.target = group;
+    contactBox.put(contact);
+  }
+
+  void deleteContact(int id) {
+    contactBox.remove(id);
+  }
+
+  List<Group> getGroups() => groupBox.getAll();
+}
